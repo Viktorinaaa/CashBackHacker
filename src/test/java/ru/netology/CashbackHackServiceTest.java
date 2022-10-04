79e5f1333254d9d33c7dd54baa8bf58523b97154
Viki <masalikavika@yandex.ru>
2022-10-04 22:36:50 +0300

    add tests

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -20,7 +20,7 @@ public class CashbackHackServiceTest {
 
     @Test
     public void remainTest2() {
-        int expected = 1000;
+        int expected = 0;
         int actual = service.remain(1000);
         ;
         assertEquals(actual, expected);
